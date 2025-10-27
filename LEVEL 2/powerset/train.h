f7d3c055ca02fb9710e81f4bbeaa8b4f1d80963a
Yoni Bensoussan <yonibnss@gmail.com>
2025-10-27 23:31:11 +0100

    1

--- LEVEL 2/powerset/train.h
+++
@@ -1,6 +0,0 @@
-#ifndef TRAIN_H
-#define TRAIN_H
-
-void find_sub(int *arr, int size, int target, int *sub, int sub_size, int i, int sum);
-
-#endif
--- LEVEL 2/powerset/trainer.h
+++
@@ -1,6 +0,0 @@
-#ifndef TRAINER_H
-#define TRAINER_H
-
-void powerset(int *arr, int size, int target, int *sub, int sub_size, int i, int sum);
-
-#endif
